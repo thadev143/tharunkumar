892f3282c4df449577de21e9b9341581d6fa5a85
ratata <mrtharun@gmail>
2024-10-06 16:08:13 +0000

    solution

---
+++ src/main/java/com/example/nxtstayz/repository/RoomJpaRepository.java
@@ -0,0 +1,11 @@
+package com.example.nxtstayz.repository;
+
+import java.util.*;
+import com.example.nxtstayz.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomJpaRepository extends JpaRepository<Room, Integer> {
+
+}
